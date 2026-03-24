41116c1a3d8db500d9db2a002d7e1907792ba522
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2026-03-24 03:12:30 -0400

    fix(entity) change AUTO to IDENTITY

--- src/main/java/com/trevisan/CalculadoraMicroServicesDB/Domain/Operations.java
+++ src/main/java/com/trevisan/CalculadoraMicroServicesDB/Domain/Operations.java
@@ -21,7 +21,7 @@ import java.time.LocalDateTime;
 public class Operations {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long operationsId;
 
     private String valueOne;
