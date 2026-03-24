b171e2d8847a318bb3ae9d084c9540b56b5648c7
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2026-03-24 03:57:56 -0400

    fix(repository) correcting query and create tables file

--- src/main/resources/DBQuerys/MySQL/V1_CREATE_TABLES_DB.sql
+++ src/main/resources/DBQuerys/MySQL/V1_CREATE_TABLES_DB.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `operations` (
     `valueOne` VARCHAR(255),
     `valueTwo` VARCHAR(255),
     `tipoDeOperacao` INT NOT NULL,
+    `result` VARCHAR(255),
     `timeOperation` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY(`id`)
 );
